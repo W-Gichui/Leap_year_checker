36f409a163ba4f231550b3233ae26d9409e6e858
Gichui <wycliffgichui@gmail.com>
2022-02-13 23:11:17 +0300

    Sequence checker code

---
+++ Fibchecker.py
@@ -0,0 +1,18 @@
+num = input("Input number...")
+y = int(num)
+def fibrinolysis(n):
+    global sequence
+    sequence = [0, 1]
+    a = 0
+    b = 1
+    global c
+    for i in range (2, n) :
+        c = a + b
+        a = b
+        b = c   
+        sequence.append(c)
+fibrinolysis(9999)
+if y in sequence :
+            print("Present")
+else:
+            print("Absent")
